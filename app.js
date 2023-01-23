f2ef05c88d0ab0f1f9b2a71f0154ed835288dd36
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-01-24 00:54:28 +0100

    some minor changes and fix circural dependiency

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const Express = require("express");
+const { connectToDb, getDb } = require("./db");
 
 const app = Express();
-
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Listening at the port nr ${PORT}`)
-});
+//connecting to DB
+let db
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(PORT, () => {
+      console.log(`Listening at the port nr ${PORT}`)
+    })
+    db = getDb()
+  }
+})
+
+
 
 app.get('/books', (req, res) => {
   res.json('Welcome to my library')
--- db.js
+++ db.js
@@ -1,15 +1,17 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb')
 
 let dbConnection
 
 module.exports = {
-  connectToDb: () => {
+  connectToDb: (cb) => {
     MongoClient.connect('mongodb://localhost:27017/bookstore')
-      .then((client) => {
+      .then(client => {
         dbConnection = client.db()
+        return cb()
       })
       .catch(err => {
         console.log(err)
+        return cb(err)
       })
   },
   getDb: () => dbConnection
