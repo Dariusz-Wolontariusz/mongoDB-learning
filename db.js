b5adf63a298151aab206e4fecb2ccf0a06abaab0
Dariusz-Wolontariusz <darek.ciazynski@protonmail.com>
2023-01-30 17:18:49 +0100

    added connection to Atlas / uri need to be changed manually. Project finished

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
+const uri = 'mongodb+srv://Darek:<F0r00m0wicz>@cluster0.cwxx3vq.mongodb.net/?retryWrites=true&w=majority'
+const compass = 'mongodb://127.0.0.1:27017/Bookstore'
 
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect('mongodb://127.0.0.1:27017/Bookstore')
+    MongoClient.connect(compass)
       .then(client => {
         dbConnection = client.db()
         return cb()
